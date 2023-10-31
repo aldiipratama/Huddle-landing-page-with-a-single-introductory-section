63fff7165dfa15f0f19eb0fdcd0a8b48d9a05950
aldiipratama <paldi0013@gmail.com>
2023-10-31 15:32:35 +0700

    challange

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+        center: true,
+    },
+    extend: {
+        colors: {
+            'violet': '#674baf',
+            'soft-magenta': '#e882e8'
+        }
+    },
+  },
+  plugins: [],
+}
+
